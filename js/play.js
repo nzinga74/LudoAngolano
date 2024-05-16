67954e5d28f723b52c0b137829c1c6eda65d48d5
nzinga74 <nzingaenoque@gmail.com>
2024-05-16 06:11:26 +0100

    first commit

---
+++ js/play.js
@@ -0,0 +1,558 @@
+const PLAYER_PATHS = {
+  0: {
+    0: 4,
+    1: 5,
+    2: 6,
+    3: 7,
+    4: 8,
+    5: 9,
+    6: 10,
+    7: 11,
+    8: 12,
+    9: 13,
+    10: 14,
+    11: 15,
+    12: 16,
+    13: 17,
+    14: 18,
+    15: 19,
+    16: 20,
+    17: 21,
+    18: 22,
+    19: 23,
+    20: 24,
+    21: 25,
+    22: 26,
+    23: 27,
+    24: 28,
+    25: 29,
+    26: 30,
+    27: 31,
+    28: 32,
+    29: 33,
+    30: 34,
+    31: 35,
+    32: 36,
+    33: 37,
+    34: 38,
+    35: 39,
+    36: 40,
+    37: 41,
+    38: 42,
+    39: 43,
+    40: 44,
+    41: 45,
+    42: 46,
+    43: 47,
+    44: 48,
+    45: 49,
+    46: 50,
+    47: 51,
+    48: 52,
+    49: 53,
+    50: 54,
+    51: 55,
+    52: 56,
+    53: 57,
+    54: 58,
+    55: 59,
+    56: 60,
+    57: 61,
+    58: 62,
+    59: 63,
+    60: 64,
+    61: 65,
+    62: 66,
+    63: 67,
+    64: 68,
+    65: 69,
+    66: 70,
+    67: 71,
+    68: 72,
+    69: 73,
+    70: 74,
+    71: 75,
+    72: 76,
+    73: 77,
+    74: 78,
+    75: 79,
+    76: 80,
+    77: 81,
+    78: 82,
+    79: 83,
+    600: 600,
+    601: 601,
+    602: 602,
+    603: 603,
+  },
+  1: {
+    0: 25,
+    1: 26,
+    2: 27,
+    3: 28,
+    4: 29,
+    5: 30,
+    6: 31,
+    7: 32,
+    8: 33,
+    9: 34,
+    10: 35,
+    11: 36,
+    12: 37,
+    13: 38,
+    14: 39,
+    15: 40,
+    16: 41,
+    17: 42,
+    18: 43,
+    19: 44,
+    20: 45,
+    21: 46,
+    22: 47,
+    23: 48,
+    24: 49,
+    25: 50,
+    26: 51,
+    27: 52,
+    28: 53,
+    29: 54,
+    30: 55,
+    31: 56,
+    32: 57,
+    33: 58,
+    34: 59,
+    35: 60,
+    36: 61,
+    37: 62,
+    38: 63,
+    39: 64,
+    40: 65,
+    41: 66,
+    42: 67,
+    43: 68,
+    44: 69,
+    45: 70,
+    46: 71,
+    47: 72,
+    48: 73,
+    49: 74,
+    50: 75,
+    51: 76,
+    52: 77,
+    53: 78,
+    54: 79,
+    55: 80,
+    56: 81,
+    57: 82,
+    58: 83,
+    59: 0,
+    60: 1,
+    61: 2,
+    62: 3,
+    63: 4,
+    64: 5,
+    65: 6,
+    66: 7,
+    67: 8,
+    68: 9,
+    69: 10,
+    70: 11,
+    71: 12,
+    72: 13,
+    73: 14,
+    74: 15,
+    75: 16,
+    76: 17,
+    77: 18,
+    78: 19,
+    79: 20,
+    700: 700,
+    701: 701,
+    702: 702,
+    703: 703,
+  },
+  2: {
+    0: 46,
+    1: 47,
+    2: 48,
+    3: 49,
+    4: 50,
+    5: 51,
+    6: 52,
+    7: 53,
+    8: 54,
+    9: 55,
+    10: 56,
+    11: 57,
+    12: 58,
+    13: 59,
+    14: 60,
+    15: 61,
+    16: 62,
+    17: 63,
+    18: 64,
+    19: 65,
+    20: 66,
+    21: 67,
+    22: 68,
+    23: 69,
+    24: 70,
+    25: 71,
+    26: 72,
+    27: 73,
+    28: 74,
+    29: 75,
+    30: 76,
+    31: 77,
+    32: 78,
+    33: 79,
+    34: 80,
+    35: 81,
+    36: 82,
+    37: 83,
+    38: 0,
+    39: 1,
+    40: 2,
+    41: 3,
+    42: 4,
+    43: 5,
+    44: 6,
+    45: 7,
+    46: 8,
+    47: 9,
+    48: 10,
+    49: 11,
+    50: 12,
+    51: 13,
+    52: 14,
+    53: 15,
+    54: 16,
+    55: 17,
+    56: 18,
+    57: 19,
+    58: 20,
+    59: 21,
+    60: 22,
+    61: 23,
+    62: 24,
+    63: 25,
+    64: 26,
+    65: 27,
+    66: 28,
+    67: 29,
+    68: 30,
+    69: 31,
+    70: 32,
+    71: 33,
+    72: 34,
+    73: 35,
+    74: 36,
+    75: 37,
+    76: 38,
+    77: 39,
+    78: 40,
+    79: 41,
+    800: 800,
+    801: 801,
+    802: 802,
+    803: 803,
+  },
+  3: {
+    0: 67,
+    1: 68,
+    2: 69,
+    3: 70,
+    4: 71,
+    5: 72,
+    6: 73,
+    7: 74,
+    8: 75,
+    9: 76,
+    10: 77,
+    11: 78,
+    12: 79,
+    13: 80,
+    14: 81,
+    15: 82,
+    16: 83,
+    17: 0,
+    18: 1,
+    19: 2,
+    20: 3,
+    21: 4,
+    22: 5,
+    23: 6,
+    24: 7,
+    25: 8,
+    26: 9,
+    27: 10,
+    28: 11,
+    29: 12,
+    30: 13,
+    31: 14,
+    32: 15,
+    33: 16,
+    34: 17,
+    35: 18,
+    36: 19,
+    37: 20,
+    38: 21,
+    39: 22,
+    40: 23,
+    41: 24,
+    42: 25,
+    43: 26,
+    44: 27,
+    45: 28,
+    46: 29,
+    47: 30,
+    48: 31,
+    49: 32,
+    50: 33,
+    51: 34,
+    52: 35,
+    53: 36,
+    54: 37,
+    55: 38,
+    56: 39,
+    57: 40,
+    58: 41,
+    59: 42,
+    60: 43,
+    61: 44,
+    62: 45,
+    63: 46,
+    64: 47,
+    65: 48,
+    66: 49,
+    67: 50,
+    68: 51,
+    69: 52,
+    70: 53,
+    71: 54,
+    72: 55,
+    73: 56,
+    74: 57,
+    75: 58,
+    76: 59,
+    77: 60,
+    78: 61,
+    79: 62,
+    900: 900,
+    901: 901,
+    902: 902,
+    903: 903,
+    904: 904,
+  },
+};
+
+var players = [
+  [600, 601, 602, 603],
+  [700, 701, 702, 703],
+  [800, 801, 802, 803],
+  [900, 901, 902, 903],
+];
+
+var players_info = {
+  1: {
+    livePiece: 0,
+    existPieceLive: false,
+  },
+  2: {
+    livePiece: 0,
+    existPieceLive: false,
+  },
+  3: {
+    livePiece: 0,
+    existPieceLive: false,
+  },
+  4: {
+    livePiece: 0,
+    existPieceLive: false,
+  },
+};
+
+var num_players = 4;
+var actualPlayer = 1;
+var currentPlayer = 1;
+var turns = [];
+var turnIndex = 0;
+var playAgain = false;
+var selectedPriorityDice = 1;
+
+function getPlace(position) {
+  return PLAYER_PATHS[actualPlayer - 1][position];
+}
+
+function isDiceEmpty(turns) {
+  if (turns.length > 0) return false;
+  return true;
+}
+
+function incrementActualPlayer(min) {
+  removeSelectedUser(actualPlayer, min);
+  actualPlayer += 1;
+  if (actualPlayer > 4) {
+    actualPlayer = 1;
+  }
+  selectActualUser(actualPlayer, min);
+}
+
+function changePiecePlace(pieceId, place) {
+  var placeId = `id-${place}`;
+  var placeElement = document.getElementById(placeId);
+  var pieceElement = document.getElementById(pieceId);
+  placeElement.append(pieceElement);
+}
+async function onchoosePiece(id, piece, player) {
+  if (player != actualPlayer) return;
+  if (isDiceEmpty(turns)) return;
+  var position = players[actualPlayer - 1][piece - 1];
+  //Incrementar posicao
+  if (turns[0] == turns[1]) {
+    playAgain = true;
+  }
+
+  if (position >= 600) {
+    var turn = getDice();
+    if (turn == 6) {
+      players[actualPlayer - 1][piece - 1] = 0;
+      var place = getPlace(0);
+      changePiecePlace(id, place);
+    } else {
+      turns = [turn, ...turns];
+    }
+  } else {
+    var turn = getDice();
+    //incrementPosition(id, piece, turn);
+    await movePiece(id, piece, turn);
+  }
+
+  if (isDiceEmpty(turns)) {
+    incrementActualPlayer(1000);
+  }
+}
+
+function randomRollDice() {
+  if (!isDiceEmpty(turns)) return;
+  var isLive = players_info[actualPlayer].existPieceLive;
+  var firstRandomDieceNumber = Math.floor(Math.random() * 6) + 1;
+  var secondRandomDieceNumber = Math.floor(Math.random() * 6) + 1;
+  if (secondRandomDieceNumber == 6) {
+    var aux = secondRandomDieceNumber;
+    secondRandomDieceNumber = firstRandomDieceNumber;
+    firstRandomDieceNumber = aux;
+  }
+  if (!isLive) {
+    if (secondRandomDieceNumber == 6 || firstRandomDieceNumber == 6) {
+      players_info[actualPlayer].existPieceLive = true;
+      turns.push(firstRandomDieceNumber, secondRandomDieceNumber);
+    } else {
+      incrementActualPlayer(4110);
+      turns = [];
+    }
+  } else {
+    turns.push(firstRandomDieceNumber, secondRandomDieceNumber);
+  }
+  rollDace(firstRandomDieceNumber, "dice-1");
+  rollDace(secondRandomDieceNumber, "dice-2");
+}
+
+function getDice() {
+  if (selectedPriorityDice == 1) {
+    return turns.shift();
+  }
+  return turns.pop();
+}
+function rollDace(dieceNumber, diceId) {
+  var dice = document.getElementById(diceId);
+  dice.style.animation = "rolling 4s";
+  setTimeout(() => {
+    switch (dieceNumber) {
+      case 1:
+        dice.style.transform = "rotateX(0deg) rotateY(0deg)";
+        break;
+      case 2:
+        dice.style.transform = "rotateX(-90deg) rotateY(0deg)";
+        break;
+      case 3:
+        dice.style.transform = "rotateX(0deg) rotateY(90deg)";
+        break;
+      case 4:
+        dice.style.transform = "rotateX(0deg) rotateY(-90deg)";
+        break;
+      case 5:
+        dice.style.transform = "rotateX(90deg) rotateY(0deg)";
+        break;
+      case 6:
+        dice.style.transform = "rotateX(180deg) rotateY(0deg)";
+        break;
+      default:
+        break;
+    }
+    dice.style.animation = "none";
+  }, 4050);
+}
+
+window.onload = selectActualPlayerOnLoad(1);
+
+function changePriorityDice() {
+  var id = `priority-dice`;
+  var removePriorityDiceId = `${id}-1`;
+  var addPriorityDiceId = `${id}-2`;
+
+  if (selectedPriorityDice == 1) {
+    selectedPriorityDice = 2;
+  } else {
+    removePriorityDiceId = `${id}-2`;
+    addPriorityDiceId = `${id}-1`;
+    selectedPriorityDice = 1;
+  }
+
+  document
+    .getElementById(removePriorityDiceId)
+    .classList.remove("priority-dice-selected");
+  document
+    .getElementById(addPriorityDiceId)
+    .classList.add("priority-dice-selected");
+}
+function selectActualPlayerOnLoad(playerId) {
+  var id = `player${playerId}`;
+  var selectedPlayer = document.getElementById(id);
+  selectedPlayer.classList.add("select-player-animation");
+}
+function selectActualUser(playerId, min) {
+  setTimeout(() => {
+    var id = `player${playerId}`;
+    var selectedPlayer = document.getElementById(id);
+    selectedPlayer.classList.add("select-player-animation");
+  }, min);
+}
+
+function removeSelectedUser(playerId, min) {
+  setTimeout(() => {
+    var id = `player${playerId}`;
+    var playerToRemove = document.getElementById(id);
+    playerToRemove.classList.remove("select-player-animation");
+  }, min);
+}
+
+async function movePiece(pieceId, pieceNumber, turn) {
+  var increment = 1;
+  return await new Promise((resolve) => {
+    var moveInterval = setInterval(() => {
+      incrementPosition(pieceId, pieceNumber, 1);
+      ++increment;
+      if (increment > turn) {
+        clearInterval(moveInterval);
+        resolve(true);
+      }
+    }, 200);
+  });
+}
+
+function incrementPosition(pieceId, piece, turn) {
+  players[actualPlayer - 1][piece - 1] += turn;
+  var place = getPlace(players[actualPlayer - 1][piece - 1]);
+  changePiecePlace(pieceId, place);
+}
